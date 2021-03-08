a1769b61670df73b774b3676e118d2ad41659836
pgoggles <praneeth.gogineni@gmail.com>
2021-03-08 16:46:25 -0700

    Rough Version of JSONStringify

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,6 +4,42 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
+  var result = '';
+  if (typeof obj === 'undefined' || typeof obj === 'function') {
+    return result;
+  }
+  if (obj === null) {
+    return result += null;
+  }
+  if (typeof obj === 'number' || typeof obj === 'boolean' || typeof obj === 'bigint') {
+    return result += obj;
+  }
+  if (typeof obj === 'string') {
+    return result += '"' + obj + '"';
+  }
+  if (Array.isArray(obj)) {
+    result += '[';
+    for (var i = 0; i < obj.length; i++) {
+      if (i !== obj.length - 1) {
+        result += stringifyJSON(obj[i]) + ',';
+      } else {
+        return result += stringifyJSON(obj[i]) + ']';
+      }
+    }
+  }
+  if (typeof obj === 'object') {
+    var lastKey = Object.keys(obj)[Object.keys(obj.length - 1)];
+    result += '{';
+    for (var key in obj) {
+      result += '"' + key + '" : ';
+      result += stringifyJSON(obj[key]);
+      if (key !== lastKey) {
+        result += ', ';
+      }
+    }
+    return result += '}';
+  }
+  return result;
 };
 
 // Edgar's Pseudocode
@@ -35,15 +71,17 @@ var stringifyJSON = function(obj) {
 //    objLength = result.length //store length to determine if last
 //    for i=0, i<objLength; i++
 //      result += stringifyJSON(obj[objLength])
-//      if i !== length
+//      if i !== length - 1
 //          result += ',' //add comma if not last
 //    result += ']'
 // if (type === object)
-//    var lastObject = Object.keys(obj)[Object.keys(obj).length - 1] // store lastObject as a variable
+//    var lastKey = Object.keys(obj)[Object.keys(obj).length - 1] // store lastObject as a variable
 //    result += '{'
 //    for var key in obj
 //        result += stringifyJSON(key) // does this need to be stringified?
 //        result += stringifyJSON(obj[key]) // stringify the value
+//        if key !== lastKey
+//            result += ','
 //    result += '}'
 // if (type === function or type === undefined)
 //    return undefined
