931cc2691f006c9e253ff3a591dace3cb21ab366
pgoggles <praneeth.gogineni@gmail.com>
2021-03-08 17:02:15 -0700

    stringifyJSON completed

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -17,6 +17,7 @@ var stringifyJSON = function(obj) {
   if (typeof obj === 'string') {
     return result += '"' + obj + '"';
   }
+
   if (Array.isArray(obj)) {
     if (obj.length === 0) {
       return result += '[]';
@@ -30,63 +31,20 @@ var stringifyJSON = function(obj) {
       }
     }
   }
+
   if (typeof obj === 'object') {
     var lastKey = Object.keys(obj)[Object.keys(obj).length - 1];
-    console.log(lastKey);
     result += '{';
     for (var key in obj) {
-      result += '"' + key + '":';
-      result += stringifyJSON(obj[key]);
-      if (key !== lastKey) {
-        result += ',';
+      if (typeof obj[key] !== 'undefined' && typeof obj[key] !== 'function') {
+        result += '"' + key + '":';
+        result += stringifyJSON(obj[key]);
+        if (key !== lastKey) {
+          result += ',';
+        }
       }
     }
     return result += '}';
   }
   return result;
 };
-
-// Edgar's Pseudocode
-// make a result variable
-// check if obj is a primitve value
-//    result is = obj
-// if obj is array
-//    result += '['
-//    iterate through array
-//    result += invoke stringify on current element
-//    when finished looping, result += ']'
-// if obj is an Object
-//    iterate through keys
-//    result += '{'
-//    invoke stringify on the values of each key
-//    result += '"' + key + ':' + stringifyJSON(value) + ','
-//    when finished looping result += '}'
-
-// Praneeth's Pseudocode
-// create result string
-// if (type===number)
-//    result += number.toString (do we need to toString?)
-// if (type === boolean)
-//    result += boolean
-// if (type === string)
-//    result += '"' + string + '"' (single or double quotes??)
-// if (Array.isArray(obj))
-//    result += '['
-//    objLength = result.length //store length to determine if last
-//    for i=0, i<objLength; i++
-//      result += stringifyJSON(obj[objLength])
-//      if i !== length - 1
-//          result += ',' //add comma if not last
-//    result += ']'
-// if (type === object)
-//    var lastKey = Object.keys(obj)[Object.keys(obj).length - 1] // store lastObject as a variable
-//    result += '{'
-//    for var key in obj
-//        result += stringifyJSON(key) // does this need to be stringified?
-//        result += stringifyJSON(obj[key]) // stringify the value
-//        if key !== lastKey
-//            result += ','
-//    result += '}'
-// if (type === function or type === undefined)
-//    return undefined
-// return result
