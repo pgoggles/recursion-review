86adbaa6f79cfe9f91fedf9640c3b96271fb932a
Eddie <elremolino91@gmail.com>
2021-03-08 21:00:14 -0800

    complete get elementsbyclassname

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,41 +5,22 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className) {
-  //
-  // var classList = element.classList;
-  // var childNodes = element.childNodes;
   var result = [];
-  var element = document.body;
-  // for (var key in classList) {
-  //   if (classList[key] === className) {
-  //     result.push(classList[key]);
-  //   }
-  // }
-
   var checkForClassName = function (element) {
     var classList = element.classList;
     var childNodes = element.childNodes;
-    for (var key in classList) {
-      if (classList[key] === className) {
-        result.push(classList[key]);
+    if (classList !== undefined) {
+      for (var i = 0; i < classList.length; i++) {
+        if (classList[i] === className) {
+          result.push(element);
+        }
       }
     }
     for (var key in childNodes) {
       checkForClassName(childNodes[key]);
     }
   };
-
-  checkForClassName(element);
-  console.log(result);
+  checkForClassName(document.body);
   return result;
 };
 
-
-
-// var checkChildNodes (passing in an object)
-// loop thru childNodes
-//    define each childNode = childnodes[key]
-//    check classlist of childNode
-//      if we match array.push(childNode)
-//    check if childNodes has children of its own
-//      if has children invoke checkChildNodes(childNode)
